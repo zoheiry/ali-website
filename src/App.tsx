7d426c7a3a96332a03a992569a582d22b56df3bb
zoheiry <ali.elzoheiry@gmail.com>
2022-02-08 15:59:25 +0100

    Add full-page scroll package

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FullPage, Slide } from 'react-full-page';
 import Classes from './App.module.scss';
-import profileImageSrc from './images/my-avatar.png';
-
 
 const App = () => {
   return (
     <div className={Classes.root}>
-      <div className="intro">
-        <div>
-          <div className="profileImageRoot">
-            <img src={profileImageSrc} />
-          </div>
-        </div>
-        <p className="introText">
-          Hello, my name is Ali Elzoheiry, I am a software engineer with 8+ years of experience.
-          <br />
-          I love building products and working with fun and passionate people to bring our ideas to life.
-        </p>
-      </div>
+      <FullPage>
+        <Slide className={Classes.page}>
+          <div>Page 1</div>
+        </Slide>
+        <Slide className={Classes.page}>
+          <div>Page 2</div>
+        </Slide>
+      </FullPage>
     </div>
   );
 };
