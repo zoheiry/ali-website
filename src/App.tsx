fc7acc781b42aa08dac4fb1f71ab515de4bab2de
zoheiry <ali.elzoheiry@gmail.com>
2022-02-09 22:02:04 +0100

    Use different scroll lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import React, { useEffect } from 'react';
-import { FullPage, Slide } from 'react-full-page';
+import ReactFullpage from '@fullpage/react-fullpage';
+import IntroSlide from './components/IntroSlide/IntroSlide';
+
 import Classes from './App.module.scss';
 
 const App = () => {
   return (
     <div className={Classes.root}>
-      <FullPage>
-        <Slide className={Classes.page}>
-          <div>Page 1</div>
-        </Slide>
-        <Slide className={Classes.page}>
-          <div>Page 2</div>
-        </Slide>
-      </FullPage>
+      <ReactFullpage duration={600}
+        easingcss3="ease-out"
+        onLeave={(_, { index }) => console.log(`going to slide ${index}`)}
+        render={({ state, fullpageApi }) => {
+          console.log(state);
+          return(
+            <ReactFullpage.Wrapper>
+              <div className={`${Classes.page} section`}>
+                <pre>{JSON.stringify(state.origin?.index, null, 2)}</pre>
+              </div>
+              <div className={`${Classes.page} section`}>
+                <pre>{JSON.stringify(state.origin?.index, null, 2)}</pre>
+              </div>
+              <div className={`${Classes.page} section`}>
+                <pre>{JSON.stringify(state.origin?.index, null, 2)}</pre>
+              </div>
+              <div className={`${Classes.page} section`}>
+                <pre>{JSON.stringify(state.origin?.index, null, 2)}</pre>
+              </div>
+            </ReactFullpage.Wrapper>
+          );}}
+      />
+      
     </div>
   );
 };
---
+++ src/components/IntroSlide/IntroSlide.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Classes from './IntroSlide.module.scss';
+
+const IntroSlide = () => (
+  <div className={Classes.root}>
+    <h1>ALI ELZOHEIRY</h1>
+  </div>
+);
+
+export default IntroSlide;
