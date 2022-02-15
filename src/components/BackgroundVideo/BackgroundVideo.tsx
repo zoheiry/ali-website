d4c6c01610e3202c846bd0a0537cbb2b78c1d3ae
zoheiry <ali.elzoheiry@gmail.com>
2022-02-15 17:21:03 +0100

    Add ovelay

--- src/components/BackgroundVideo/BackgroundVideo.tsx
+++ src/components/BackgroundVideo/BackgroundVideo.tsx
@@ -5,19 +5,22 @@ import Classes from './BackgroundVideo.module.scss';
 type Props = {
   src: string;
   type?: string;
+  showOverlay?: boolean;
 }
 
-const BackgroundVideo = ({ src, type = 'video/mp4' } : Props) => (
-  <video
-    className={Classes.root}
-    autoPlay
-    loop
-    muted
-    data-keepplaying
-    playsInline
-  >
-    <source src={src} type={type} />
-  </video>
+const BackgroundVideo = ({ src, type = 'video/mp4', showOverlay = true } : Props) => (
+  <div className={Classes.root}>
+    {showOverlay && <div className={Classes.overlay} />}
+    <video
+      autoPlay
+      loop
+      muted
+      data-keepplaying
+      playsInline
+    >
+      <source src={src} type={type} />
+    </video>
+  </div>
 );
 
 export default BackgroundVideo;
