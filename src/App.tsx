f1ea9db3933e4981c9ddfed7d0e068cc61a728ec
zoheiry <ali.elzoheiry@gmail.com>
2022-02-18 23:28:56 +0100

    Tweak animations

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,16 @@ import React, { useState } from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
 
 import Slide from './components/Slide/Slide';
-import Intro from './components/Slides/Intro/Intro';
-import About from './components/Slides/About/About';
-import City from './components/Slides/City/City';
-import Projects from './components/Slides/Projects/Projects';
-import Contact from './components/Slides/Contact/Contact';
 import NavIndicators from './components/NavIndicators/NavIndicators';
 
 import slidesConfig from './slidesConfig';
-
-import Classes from './App.module.scss';
 
 const App = () => {
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
   const slideIds = slidesConfig.map(slide => slide.id);
 
   return (
-    <div className={Classes.root}>
+    <div>
       <NavIndicators
         numberOfSlides={slidesConfig?.length}
         activeSlideIndex={activeSlideIndex}
